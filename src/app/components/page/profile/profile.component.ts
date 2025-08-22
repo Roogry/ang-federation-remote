88521a20b8431534ecd0dfe414fc5025efed5954
BETAVIAN <68832671+UsdBetaV@users.noreply.github.com>
2025-08-22 10:55:35 +0700

    feat: update user page

--- src/app/components/page/profile/profile.component.ts
+++ src/app/components/page/profile/profile.component.ts
@@ -2,6 +2,10 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+export type ProfileType = 'name' | 'email' | 'provinceName' | 'regencyName' | 'districtName';
 
 export type Profile = {
   name: string,
@@ -13,7 +17,13 @@ export type Profile = {
 
 @Component({
   selector: 'app-profile',
-  imports: [ MatFormFieldModule, MatInputModule, ReactiveFormsModule ],
+  imports: [ 
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    MatButtonModule 
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
@@ -37,6 +47,29 @@ export class ProfileComponent implements OnInit{
     this.profileFormGroup.controls.provinceName.setValue(data.provinceName);
     this.profileFormGroup.controls.regencyName.setValue(data.regencyName);
     this.profileFormGroup.controls.districtName.setValue(data.districtName);
+  }
+
+  resetField(field: ProfileType) {
+    const dataJson = localStorage.getItem('user')!;
+    const data = JSON.parse(dataJson) as Profile;
+    this.profileFormGroup.controls[field].setValue(data[field]);
+    this.profileFormGroup.controls[field].markAsPristine();
+  }
+
+  cancel() {
+    const dataJson = localStorage.getItem('user')!;
+    const data = JSON.parse(dataJson) as Profile;
+    this.profileFormGroup.controls.name.setValue(data.name);
+    this.profileFormGroup.controls.email.setValue(data.email);
+    this.profileFormGroup.controls.provinceName.setValue(data.provinceName);
+    this.profileFormGroup.controls.regencyName.setValue(data.regencyName);
+    this.profileFormGroup.controls.districtName.setValue(data.districtName);
+    this.profileFormGroup.markAsPristine();
+  }
+
+  save() {
+    const data = this.profileFormGroup.value;
+    localStorage.setItem('user', JSON.stringify(data));
   }
 
 }
